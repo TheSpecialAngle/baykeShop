249c90121d220c78aa3e26224adb4b77e98bafd5
拜客 <1158920674@qq.com>
2023-02-21 10:30:53 +0000

    后台布局

--- baykeshop/admin/base.py
+++ baykeshop/admin/base.py
@@ -8,6 +8,16 @@ class BaseModelAdmin(admin.ModelAdmin, CustomColumns):
     重写并新增了一些全局方法
     """
 
+    class Media:
+        css = {
+            "all": ("baykeshop/css/buefy.min.css",)
+        }
+        js = (
+            "baykeshop/js/vue.js",
+            "baykeshop/js/buefy.min.js",
+            "baykeadmin/js/request.js",
+        )
+    
     # change_list_template = "baykeAdmin/change_list.html"
     actions = ["mark_delete", "export_as_csv"]
 
@@ -16,3 +26,6 @@ class BaseModelAdmin(admin.ModelAdmin, CustomColumns):
         if self.model._meta.model_name not in ['user', 'group', 'permission', 'logentry']:
             return queryset.filter(is_del=False)
         return queryset
+    
+    def media(self) -> Media:
+        return super().media
--- baykeshop/conf/sites.py
+++ baykeshop/conf/sites.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
 from django.contrib.admin.apps import AdminConfig
+from django import forms
+from django.apps import apps
 
 
 class BaykeAdminSite(admin.AdminSite):
@@ -14,7 +16,32 @@ class BaykeAdminSite(admin.AdminSite):
 
     # Text to put at the top of the admin index page.
     index_title = gettext_lazy("Site administration")
-
+    
+    index_template = "baykeadmin/index.html"
+    
+    class Media:
+        css = {
+            "all": ("baykeshop/css/buefy.min.css",)
+        }
+        js = (
+            "baykeshop/js/vue.js",
+            "baykeshop/js/buefy.min.js",
+            "baykeadmin/js/request.js",
+        )
+    
+    def get_app_list(self, request, app_label=None):
+        return super().get_app_list(request)
+    
+    def index(self, request, extra_context=None):
+        extra_context = {"media": self.media}
+        return super().index(request, extra_context)
+    
+    def each_context(self, request):
+        return super().each_context(request)
+    
+    @property
+    def media(self):
+        return forms.Media(self.Media)
 
 
 class BaykeAdminConfig(AdminConfig):
