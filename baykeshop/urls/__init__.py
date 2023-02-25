2c522544a300e81cba94afa6f041cb40908c17fd
拜客 <1158920674@qq.com>
2023-02-25 14:44:57 +0000

    文档新增

--- baykeshop/urls/__init__.py
+++ baykeshop/urls/__init__.py
@@ -1,5 +1,7 @@
-from .shop import app_name
+from .shop import app_name, path
 from .shop import urlpatterns
+from baykeshop.views.docs import index
 
-
-
+urlpatterns += [
+    path("docs/", index.HomeView.as_view(), name="doc_index")
+]
