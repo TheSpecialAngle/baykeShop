ee9c457bbdb7e6f9ce49821fe38edc2e9d7d69f3
轻编程 <2539909370@qq.com>
2023-03-17 11:01:00 +0000

    导出json数据

--- bayke/settings.py
+++ bayke/settings.py
@@ -76,17 +76,11 @@ WSGI_APPLICATION = 'bayke.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'baykeshopdb',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'OPTIONS': {
+            'read_default_file': f'{BASE_DIR}/mysql.cnf',
+        },
     }
 }
 
