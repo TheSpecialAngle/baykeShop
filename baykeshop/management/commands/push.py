0d4f87c51cb803b8d6670b864a85700bfbf0b2b6
轻编程 <2539909370@qq.com>
2023-03-19 09:25:05 +0000

    push 命令完善

--- baykeshop/management/commands/push.py
+++ baykeshop/management/commands/push.py
@@ -9,13 +9,12 @@ from baykeshop.models import BaykePermission, BaykeMenu
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = '初始化后台管理菜单以及导入测试数据'
 
     def add_arguments(self, parser):
         parser.add_argument('data', nargs='?', type=str)
-
+        
     def handle(self, *args, **options):
-        print(options)
         if options['data'] is None:
             menus_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykemenu.json"
             management.call_command('loaddata', menus_json, verbosity=0)
@@ -26,7 +25,26 @@ class Command(BaseCommand):
                     BaykePermission.objects.update_or_create(permission=perm, menus=menus.filter(name="商城").first(), defaults={'permission': perm})
                 elif perm.codename in ['view_group', 'view_user', 'view_baykemenu', 'view_logentry']:
                     BaykePermission.objects.update_or_create(permission=perm, menus=menus.filter(name="认证和授权").first(), defaults={'permission': perm})
-                print(perm.codename)
-                
-                
-            self.stdout.write(self.style.SUCCESS('Successfully "%s"' % 'push data'))
+                elif perm.codename in ['view_baykeshoporderinfo']:
+                    BaykePermission.objects.update_or_create(permission=perm, menus=menus.filter(name="订单").first(), defaults={'permission': perm})
+                    
+            self.stdout.write(self.style.SUCCESS('Successfully "%s"' % 'push menus'))
+            
+        elif options['data'] == 'test':
+            banners_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykebanner.json"
+            management.call_command('loaddata', banners_json, verbosity=0)
+            category_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykeshopcategory.json"
+            management.call_command('loaddata', category_json, verbosity=0)
+            
+            spec_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykeshopspec.json"
+            specoption_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykeshopspecoption.json"
+            spu_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykeshopspu.json"
+            spucarousel_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykespucarousel.json"
+            sku_json = f"{settings.BASE_DIR}/baykeshop/config/db/baykeshopsku.json"
+            
+            management.call_command('loaddata', spec_json, verbosity=0)
+            management.call_command('loaddata', specoption_json, verbosity=0)
+            management.call_command('loaddata', spu_json, verbosity=0)
+            management.call_command('loaddata', spucarousel_json, verbosity=0)
+            management.call_command('loaddata', sku_json, verbosity=0)
+            self.stdout.write(self.style.SUCCESS('Successfully "%s"' % 'push test data'))
